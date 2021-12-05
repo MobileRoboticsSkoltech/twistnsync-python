62d6050ab3e760023385322b8c4e546a9dd3d98f
anastasiia-kornilova <kornilova.anastasiia@gmail.com>
2021-12-06 00:15:02 +0300

    remove extras, overwrite module

--- twistnsync/__init__.py
+++ twistnsync/__init__.py
@@ -3,3 +3,12 @@
 __author__ = """Marsel Faizullin"""
 __email__ = 'marsel.faizullin@skoltech.ru'
 __version__ = '0.1.0'
+
+from . import twistnsync
+
+for module in dir(twistnsync):
+    n = len(module) - 1
+    if not (module[:2] == '__' and module[n:n-2:-1] == '__') and module.count('.') == 0:
+        globals()[module] = getattr(twistnsync, module)
+
+del twistnsync
